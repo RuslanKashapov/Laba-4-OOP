43695aa18246c84e64b4b3d8fcf541884f72c8f9
Руслан Кашапов <k.ruslan.ufa@gmail.com>
2021-01-08 00:18:26 +0500

    Переделывание

--- Laba №4 OOP/Form1.Designer.cs
+++ Laba №4 OOP/Form1.Designer.cs
@@ -29,31 +29,45 @@
         private void InitializeComponent()
         {
             this.PaintBox = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // PaintBox
             // 
             this.PaintBox.Location = new System.Drawing.Point(0, 1);
             this.PaintBox.Name = "PaintBox";
-            this.PaintBox.Size = new System.Drawing.Size(713, 540);
+            this.PaintBox.Size = new System.Drawing.Size(886, 607);
             this.PaintBox.TabIndex = 0;
-            this.PaintBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PaintBox_MouseClick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(892, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(113, 21);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Создавать ";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(849, 542);
+            this.ClientSize = new System.Drawing.Size(1021, 608);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.PaintBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel PaintBox;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
