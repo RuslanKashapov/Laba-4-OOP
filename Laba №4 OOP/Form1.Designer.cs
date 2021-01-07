4fef52a2c749ae1dc6aa51e86e9d7fd430193a19
Руслан Кашапов <k.ruslan.ufa@gmail.com>
2021-01-07 13:50:01 +0500

    Добавьте файлы проекта.

---
+++ Laba №4 OOP/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Laba__4_OOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PaintBox = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // PaintBox
+            // 
+            this.PaintBox.Location = new System.Drawing.Point(0, 1);
+            this.PaintBox.Name = "PaintBox";
+            this.PaintBox.Size = new System.Drawing.Size(713, 540);
+            this.PaintBox.TabIndex = 0;
+            this.PaintBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PaintBox_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 542);
+            this.Controls.Add(this.PaintBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PaintBox;
+    }
+}
+
